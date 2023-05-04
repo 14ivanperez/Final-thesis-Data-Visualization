4707ab5f4b3e61078dfbc720281ca7c6073f1e5d
Ivan <ivvanperez14@gmail.com>
2023-05-04 19:57:55 +0200

    Change format of Date and select price range

--- _layouts/final_thesis.py
+++ _layouts/final_thesis.py
@@ -5,7 +5,11 @@ import matplotlib.pyplot as plt
 # Load data from Excel in pandas DataFrame
 df = pd.read_csv("EuroNext 100 Historical Data.csv")
 
+# Change Date column format
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
 
+# Select price variable and range of date
+df_price = df.loc[(df['Date'] >= '2013-01-01') & (df['Date'] <= '2023-12-31'), ['Date', 'Price']]
 
 
 
