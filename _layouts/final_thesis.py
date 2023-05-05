a78360c0321808baf1afed3f1c3a2abb35ae01a9
Ivan <ivvanperez14@gmail.com>
2023-05-05 09:24:42 +0200

    Extract indicators we re interested

--- _layouts/final_thesis.py
+++ _layouts/final_thesis.py
@@ -8,3 +8,11 @@ ticker = "^N100"
 # Download data from Yahoo Finance
 data = yf.download(ticker, start="2013-01-01", end="2022-12-31")
 
+# Extract the closing price data
+closing_price = data["Close"]
+opening_price = data["Open"]
+high = data["High"]
+low = data["Low"]
+volume = data["Volume"]
+percent_change = (closing_price - opening_price) / opening_price * 100
+
